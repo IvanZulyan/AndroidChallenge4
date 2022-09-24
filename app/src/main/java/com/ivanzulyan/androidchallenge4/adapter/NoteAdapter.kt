10574e475a8a3e8bf7c304c1e0a6aa3040b4f602
IvanZulyan <zulyanivan11@gmail.com>
2022-09-25 03:51:15 +0700

    Initial commit

---
+++ app/src/main/java/com/ivanzulyan/androidchallenge4/adapter/NoteAdapter.kt
@@ -0,0 +1,73 @@
+package com.ivanzulyan.androidchallenge4.adapter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.findFragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.ivanzulyan.androidchallenge4.MainActivity
+import com.ivanzulyan.androidchallenge4.R
+import com.ivanzulyan.androidchallenge4.room.Note
+import com.ivanzulyan.androidchallenge4.room.NoteDatabase
+import com.ivanzulyan.androidchallenge4.databinding.ItemViewBinding
+import com.ivanzulyan.androidchallenge4.fragment.HomeFragment
+import com.ivanzulyan.androidchallenge4.viewmodel.NoteViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import java.util.ArrayList
+
+class NoteAdapter(var listNote : List<Note>): RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+    var DBNote : NoteDatabase? = null
+    lateinit var noteVm : NoteViewModel
+
+    class ViewHolder(var binding : ItemViewBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bindnote(noteItem: Note) {
+            binding.note = noteItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        noteVm = ViewModelProvider(holder.itemView.context as AppCompatActivity).get(NoteViewModel::class.java)
+
+        val data = listNote[position]
+        holder.bindnote(data)
+        holder.binding.btnDelete.setOnClickListener{
+            DBNote = NoteDatabase.getInstance(it.context)
+
+            GlobalScope.async {
+                noteVm.deleteNote(listNote[position])
+                (holder.itemView.context as MainActivity).runOnUiThread{
+                    (holder.itemView.findFragment<HomeFragment>()).getAllNote()
+                }
+            }
+        }
+
+        holder.binding.btnEdit.setOnClickListener {
+            var bundle = Bundle()
+            bundle.putSerializable("dataedit", listNote[position])
+            it.findNavController().navigate(R.id.action_homeFragment_to_editFragment,bundle)
+        }
+        holder.binding.cvNote.setOnClickListener{
+            var bundle = Bundle()
+            bundle.putSerializable("dataedit", listNote[position])
+            it.findNavController().navigate(R.id.action_homeFragment_to_detailFragment,bundle)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return  listNote.size
+    }
+
+    fun setNoteData(listNote: ArrayList<Note>)
+    {
+        this.listNote=listNote
+    }
+}
---
+++ app/src/main/java/com/ivanzulyan/androidchallenge4/fragment/EditFragment.kt
@@ -0,0 +1,70 @@
+package com.ivanzulyan.androidchallenge4.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.ivanzulyan.androidchallenge4.R
+import com.ivanzulyan.androidchallenge4.room.Note
+import com.ivanzulyan.androidchallenge4.room.NoteDatabase
+import com.ivanzulyan.androidchallenge4.databinding.FragmentEditBinding
+import com.ivanzulyan.androidchallenge4.viewmodel.NoteViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+
+class EditFragment : Fragment() {
+    private var _binding: FragmentEditBinding? = null
+    private val binding get() = _binding!!
+    var dbNote: NoteDatabase? = null
+    lateinit var noteVm : NoteViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEditBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        noteVm = ViewModelProvider(this).get(NoteViewModel::class.java)
+
+
+
+        dbNote = NoteDatabase.getInstance(requireActivity())
+        var getDataNote = arguments?.getSerializable("dataedit") as Note
+
+        binding.edtJudul.setText(getDataNote.title)
+        binding.edtCatatan.setText(getDataNote.content)
+        binding.id.setText(getDataNote.id.toString())
+
+
+        binding.btnEdit.setOnClickListener {
+            editNote()
+        }
+    }
+
+    fun editNote() {
+        var id = binding.id.text.toString().toInt()
+        var title = binding.edtJudul.text.toString()
+        var content = binding.edtCatatan.text.toString()
+
+
+        GlobalScope.async {
+            noteVm.updateNote(Note(id,title,content))
+            activity?.runOnUiThread {
+                Toast.makeText(requireActivity(), "Data berhasil di Edit", Toast.LENGTH_LONG).show()
+            }
+        }
+        findNavController().navigate(R.id.action_editFragment_to_homeFragment)
+    }
+}
